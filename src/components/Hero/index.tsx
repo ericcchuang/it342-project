ee408beacb19672ed312627508b8c0b1cab2f137
djs234 <156932230+PhoenixStev@users.noreply.github.com>
2025-11-12 20:13:59 -0500

    Reinstating something i took out in hope it unfucks shit
    I hate web development this shit sucks

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -17,7 +17,20 @@ const Hero = () => {
                 <p className="mb-12 text-base leading-relaxed! text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
                   The perfect place to help you  find a developer to create your own website 
                 </p>
-                </div>
+                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <Link
+                    href="https://nextjstemplates.com/templates/saas-starter-startup"
+                    className="rounded-xs bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                  >
+                    🔥 Get Pro
+                  </Link>
+                  <Link
+                    href="https://github.com/NextJSTemplates/startup-nextjs"
+                    className="inline-block rounded-xs bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
+                  >
+                    Star on GitHub
+                  </Link>
+              </div>
               </div>
             </div>
           </div>

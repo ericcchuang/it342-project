86100eccd36a0152fbbd3224913486bada77a73d
djs234 <156932230+PhoenixStev@users.noreply.github.com>
2025-11-12 20:22:59 -0500

    Reverting to proper bass to see if I unfuck shit

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
 
+...
+import Link from "next/link";
 const Hero = () => {
   return (
     <>
@@ -12,12 +13,17 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  Free and Open-Source Next.js Template for Startup & SaaS
                   Welcome to Spider's Web
                 </h1>
                 <p className="mb-12 text-base leading-relaxed! text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                  Startup is free Next.js template for startups and SaaS
+                  business websites comes with all the essential pages,
+                  components, and sections you need to launch a complete
+                  business website, built-with Next 13.x and Tailwind CSS.
                   The perfect place to help you  find a developer to create your own website 
                 </p>
-                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/saas-starter-startup"
                     className="rounded-xs bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
@@ -30,10 +36,11 @@ const Hero = () => {
                   >
                     Star on GitHub
                   </Link>
-              </div>
+                </div>
               </div>
             </div>
           </div>
+        </div>
         <div className="absolute right-0 top-0 z-[-1] opacity-30 lg:opacity-100">
           <svg
             width="450"
@@ -279,5 +286,4 @@ const Hero = () => {
     </>
   );
 };
-
 export default Hero;

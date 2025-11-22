c48bc69112d63424693f8e6564fd3cb3bc9158ab
ericcchuang <eyh3@njit.edu>
2025-11-22 00:37:41 -0500

    is my config file wrong

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -2,14 +2,18 @@ module.exports = {
   apps: [
     {
       name: "spiderweb",
-      script: "npm",
+      script: "node_modules/next/dist/bin/next",
       args: "start",
       env: {
         NODE_ENV: "production",
+        PORT: 3000,
       },
       env_file: "prod.env",
       exec_mode: "fork",
       instances: 1,
+      log_file: "/var/log/pm2/spiderweb-combined.log",
+      error_file: "/var/log/pm2/spiderweb-error.log",
+      out_file: "/var/log/pm2/spiderweb-out.log",
     },
   ],
 };

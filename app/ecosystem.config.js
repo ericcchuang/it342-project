0bb41e702f01bcc7daa839e1d79bb58e2e5166d2
ericcchuang <eyh3@njit.edu>
2025-11-22 00:45:08 -0500

    old config

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -2,18 +2,14 @@ module.exports = {
   apps: [
     {
       name: "spiderweb",
-      script: "node_modules/next/dist/bin/next",
+      script: "npm",
       args: "start",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
       },
       env_file: "prod.env",
       exec_mode: "fork",
       instances: 1,
-      log_file: "/var/log/pm2/spiderweb-combined.log",
-      error_file: "/var/log/pm2/spiderweb-error.log",
-      out_file: "/var/log/pm2/spiderweb-out.log",
     },
   ],
 };

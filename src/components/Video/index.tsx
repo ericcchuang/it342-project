24eb4623f22512bf09c304b5d6966d249ebec0bc
djs234 <djs234@njit.edu>
2025-12-01 14:08:23 -0500

    cutting bloat cherry-pick

---
+++ src/components/Video/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import VideoModal from "@/components/video-modal";
+import Image from "next/image";
+import { useState } from "react";
+import SectionTitle from "../Common/SectionTitle";
+
+export default function Video() {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <>
+      <section className="relative z-10 py-16 md:py-20 lg:py-28">
+        
+      </section>
+    </>
+  );
+};

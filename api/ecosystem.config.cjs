68d78e772fa60fe96a971f11d15fb614838666af
ericcchuang <eyh3@njit.edu>
2025-12-10 11:03:47 -0500

    more config file mistakes yay

--- api/ecosystem.config.cjs
+++ api/ecosystem.config.cjs
@@ -2,12 +2,12 @@ module.exports = {
   apps: [
     {
       name: "spiderwebapi",
-      script: "./api/express.js",
+      script: "./express.js",
       env: {
         NODE_ENV: "production",
         PORT: 3000,
       },
-      env_file: "prod.env",
+      env_file: ".env",
       exec_mode: "fork",
       instances: 1,
     },

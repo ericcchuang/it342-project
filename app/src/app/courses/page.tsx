7ba9c66a5464896a3b7b94bbb2f74005b496361f
djs234 <djs234@njit.edu>
2025-12-13 15:24:45 -0500

    restored the courses page to having links (yes it is so stupidly ugly its funny)

--- app/src/app/courses/page.tsx
+++ app/src/app/courses/page.tsx
@@ -1,25 +1,98 @@
 import Breadcrumb from "@/components/Common/Breadcrumb";
+
+
+
 import Contact from "@/components/courses";
+
+
+
+
 
 import { Metadata } from "next";
 
+
+
+
+
 export const metadata: Metadata = {
-  title: "Contact Page | Free Next.js Template for Startup and SaaS",
+
+
+  title: "Web Development Courses",
+
+
   description: "This is Contact Page for Startup Nextjs Template",
+
+
   // other metadata
+
+
 };
+
+
+
+
 
 const ContactPage = () => {
+
+
   return (
+
+
     <>
+
+
       <Breadcrumb
-        pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+
+
+        pageName="Courses"
+
+
+        description="Here are some links that will help you further your web development jouney!"
+
+
       />
 
+
+      <div> HTML Courses: 
+
+
+        <a href="https://www.w3schools.com/html/default.asp">Get started with HTML</a>
+
+
+      </div>
+
+
+      <div>CSS Courses:
+
+
+        <a href="https://www.w3schools.com/css/default.asp">Get started with CSS</a>
+
+
+      </div>
+
+
+      <div>Javascript Courses:
+
+
+        <a href="https://www.w3schools.com/js/default.asp">Get started with Javascript</a>
+
+
+      </div>
+
+
       <Contact />
+
+
     </>
+
+
   );
+
+
 };
+
+
+
+
 
 export default ContactPage;
